9fd540cc3280d1f54d7b7ce39cf467ab374122ca
shizhuoyuan <shizhuoyuan39@google.com>
2026-03-30 17:18:37 +0900

    wowanchengle01

--- lib/addog.dart
+++ lib/addog.dart
@@ -1,3 +1,4 @@
+late String name;
 void main (){
   String firstname='zhuoyuan';
 
@@ -6,10 +7,29 @@ void main (){
   print("name is $fullname");
 
 
+  String? Firstname; //Nullable
+  String sirname ; //non-nullable
+  print("value:$Firstname");
+  print("sirname");
+
   int num1 = 10 ;
+  double num2 = 3.141592;
  double pi =3.14;
  double num3 = num1 * pi;
  num num4 = num1 * pi;
+ 
+ print("num1 to str is ${num1.toStringAsFixed(0)}");
+ print("num2 to str is ${num2.toStringAsFixed(2)}");
+
+
+num1 = int.parse("-12");
+num2 = double.parse("123.4567");
+print(num1);
+print(num2);
+print("${num.parse("-34.56")}");
+ 
+ 
+ 
  print(num3);
  print(num4);
 
@@ -18,6 +38,10 @@ void main (){
  bool bb = (num1>0);
   bool chk = aa &bb;
  print("check result is $chk");
+
+
+ name = "jinyoung"
+  ?
 
 
 }
