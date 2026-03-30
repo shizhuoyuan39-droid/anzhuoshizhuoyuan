f526756233bebc77ee5214501794dead8b572168
shizhuoyuan <shizhuoyuan39@google.com>
2026-03-30 16:19:47 +0900

    wowanchengle

---
+++ lib/addog.dart
@@ -0,0 +1,25 @@
+void main (){
+  String firstname='zhuoyuan';
+
+  String lastname='shi';
+  String fullname = '$lastname $firstname';
+  print("name is $fullname");
+
+
+  int num1 = 10 ;
+ double pi =3.14;
+ double num3 = num1 * pi;
+ num num4 = num1 * pi;
+ print(num3);
+ print(num4);
+
+
+ bool aa = false;
+ bool bb = (num1>0);
+  bool chk = aa &bb;
+ print("check result is $chk");
+
+
+}
+
+
